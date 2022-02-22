4b6b199ff562990f6d5f051e0d7531443ddfdc4a
Mohammad Farrahi <farahim.1379@gmail.com>
2022-02-22 11:01:44 +0330

    Add Actor class with deserialization ability

--- src/main/java/ie/Cli.java
+++ src/main/java/ie/Cli.java
@@ -1,26 +1,24 @@
 package ie;
-
-
 import java.util.Scanner;
 
 public class Cli {
     private final Iemdb iemdb;
+    private final Scanner terminal;
     public Cli() {
         this.iemdb = new Iemdb();
+        this.terminal = new Scanner(System.in);
     }
     public void run() {
-        Scanner scanner = new Scanner(System.in);
         String line;
-
-        while((line = scanner.nextLine()) != null) {
-            String[] commandParts = line.split(" ", -2);
+        while((line = terminal.nextLine()) != null) {
+            String[] commandParts = line.split(" ", 2);
             execCommand(commandParts);
             String res = iemdb.getResponse();
             System.out.println(res);
         }
     }
 
-    private void execCommand(String[] commandParts) throws IllegalStateException{
+    private void execCommand(String[] commandParts) throws IllegalStateException {
         String command = commandParts[0];
 
         switch (command) {
