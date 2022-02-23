2f27af18866cc0d3a7d4096e6484329f81e0161c
Mohammad Farrahi <farahim.1379@gmail.com>
2022-02-23 11:52:04 +0330

    Upadte Actor class

--- src/main/java/ie/Actor.java
+++ src/main/java/ie/Actor.java
@@ -3,6 +3,7 @@ package ie;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.time.DateTimeException;
 import java.time.LocalDate;
 
 public class Actor {
@@ -10,20 +11,55 @@ public class Actor {
     private String name;
     private LocalDate birthDate;
     private String nationality;
+//    private static final String[] jsonFieldNames;
 
-    @JsonCreator
-    public Actor (
-            @JsonProperty(value= "id", required = true) String id,
-            @JsonProperty(value= "name", required = true) String name,
-            @JsonProperty(value= "birthDate", required = true) String birthDate,
-            @JsonProperty(value= "nationality", required = true) String nationality
-    ) {
-        // TODO: logical validation of fileds must be handled
+    // For jackson usage
+    @JsonProperty(value= "id", required = true)
+    private void setId(String id) {
+        if(Integer.parseInt(id) < 1) {
+            // TODO: throw exception
+            System.out.println("id invalid");
+        }
         this.id = id;
+    }
+    @JsonProperty(value= "name", required = true)
+    private void setName(String name) {
         this.name = name;
-        this.birthDate = LocalDate.parse(birthDate);
+    }
+    @JsonProperty(value= "birthDate", required = true)
+    private void setBirthDate(String birthDate) {
+        try {
+            this.birthDate = LocalDate.parse(birthDate);
+        }
+        catch(DateTimeException e) {
+            // TODO: throw exception
+            System.out.println("date invalid");
+        }
+    }
+    @JsonProperty(value= "nationality", required = true)
+    private void setNationality(String nationality) {
         this.nationality = nationality;
     }
+    @JsonCreator
+    private Actor(){}
+
+    public Actor (
+             String id,
+             String name,
+             String birthDate,
+             String nationality
+    ) {
+        // TODO: handle validation on name and nationality
+        setId(id);
+        setName(name);
+        setBirthDate(birthDate);
+        setNationality(nationality);
+    }
+
+    public String getId(){
+        return id;
+    }
+
     @Override
     public String toString() {
         return "Actor{" +
@@ -33,5 +69,4 @@ public class Actor {
                 '}';
     }
     // TODO: serialization methods must be added.
-    // TODO: update methodes must be added.
 }
--- src/main/java/ie/App.java
+++ src/main/java/ie/App.java
@@ -8,64 +8,8 @@ import java.time.DateTimeException;
 
 public class App {
     public static void main(String[] args) {
-//        Cli cli = new Cli();
-//        cli.run();
-//        LocalDate d = LocalDate.parse("2011-13-20");
-//        System.out.println(d);
-        String test1 = "{\"id\": \"123457896\",\"nationality\": \"UK\",\"name\": \"test\",\"birthDate\":\"2022-02-02\"}";
-        String test2 = "{\"id\": \"123457896\",\"name\": \"test\"}";
-        String test3 = "{\"id\": \"123457896\",\"nationality\": \"UK\",\"name\": \"test\",\"birthDate\":\"2022-02-02\", \"extra\":\"dummy\"}";
-        String test4 = "{\"id\": \"123457896\",\"nationality\": \"UK\",\"name\": \"test\",\"birthDate\":\"2022-02\"}";
-        String test5 = "{\"id\": \"123457896\",\"nationality\": \"UK\",\"name\": \"test\",\"birthDate\":\"2022-02\"";
-        try {
-            Actor actor1 = new ObjectMapper().readValue(test1, Actor.class);
-            System.out.println(actor1);
-        } catch (JsonMappingException e) {
-            System.out.println(e.getMessage());
-        } catch (DateTimeException e) {
-            System.out.println(e.getMessage());
-        } catch (JsonProcessingException e) {
-            System.out.println(e.getMessage());
-        }
-        try {
-            Actor actor2 = new ObjectMapper().readValue(test2, Actor.class);
-            System.out.println(actor2);
-        } catch (JsonMappingException e) {
-            System.out.println(e.getMessage());
-        } catch (DateTimeException e) {
-            System.out.println(e.getMessage());
-        } catch (JsonProcessingException e) {
-            System.out.println(e.getMessage());
-        }
-        try {
-            Actor actor3 = new ObjectMapper().readValue(test3, Actor.class);
-            System.out.println(actor3);
-        } catch (JsonMappingException e) {
-            System.out.println(e.getMessage());
-        } catch (DateTimeException e) {
-            System.out.println(e.getMessage());
-        } catch (JsonProcessingException e) {
-            System.out.println(e.getMessage());
-        }
-        try {
-            Actor actor4 = new ObjectMapper().readValue(test4, Actor.class);
-            System.out.println(actor4);
-        } catch (JsonMappingException e) {
-            System.out.println(e.getMessage());
-        } catch (DateTimeException e) {
-            System.out.println(e.getMessage());
-        } catch (JsonProcessingException e) {
-            System.out.println(e.getMessage());
-        }
-        try {
-            Actor actor5 = new ObjectMapper().readValue(test5, Actor.class);
-            System.out.println(actor5);
-        } catch (JsonMappingException e) {
-            System.out.println(e.getMessage());
-        } catch (DateTimeException e) {
-            System.out.println(e.getMessage());
-        } catch (JsonProcessingException e) {
-            System.out.println(e.getMessage());
-        }
+        Cli cli = new Cli();
+        cli.run();
+
     }
 }
