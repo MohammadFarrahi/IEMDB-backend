57f17ce20bc20d56db6940827be635f19885393b
Soroosh Mirzasarvari <sarvisoroosh@gmail.com>
2022-02-25 02:57:06 +0330

    Add serializer methods to ActorManager

--- src/main/java/ie/actor/ActorManager.java
+++ src/main/java/ie/actor/ActorManager.java
@@ -1,8 +1,11 @@
 package ie.actor;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import ie.Iemdb;
+import ie.film.Film;
 import ie.types.Constant;
 
 import java.util.ArrayList;
@@ -43,4 +46,43 @@ public class ActorManager {
     public boolean isIdValid(String id){
         return actorMap.containsKey(id);
     }
-}
+
+    public Actor getElement(String id) throws Exception{
+        if (actorMap.containsKey(id)) {
+            return actorMap.get(id);
+        }
+        throw new Exception("Actor not found");
+    }
+
+    public ArrayList<Actor> getElementList(ArrayList<String> idList) throws Exception{
+        ArrayList<Actor> res = new ArrayList<>();
+
+        for(var id: idList) {
+            res.add(getElement(id));
+        }
+        return res;
+    }
+
+    public JsonNode serializeElement(String id, Constant.SER_MODE mode){
+        try {
+            var actor = getElement(id);
+            var jsonNode = mapper.valueToTree(actor);
+            return jsonNode;
+
+        }catch (Exception e){
+            return null;
+        }
+    }
+
+    public JsonNode serializeElementList(ArrayList<String> idList, Constant.SER_MODE mode){
+        try {
+            var actorList = getElementList(idList);
+            var jsonNode = mapper.valueToTree(actorList);
+            return jsonNode;
+        }catch (Exception e){
+            return null;
+        }
+    }
+
+
+    }
