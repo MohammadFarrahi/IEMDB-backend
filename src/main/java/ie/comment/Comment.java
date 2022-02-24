69b7d0f7e26f9bbb795ccc4f72290331508f40be
Soroosh Mirzasarvari <sarvisoroosh@gmail.com>
2022-02-24 22:25:39 +0330

    Add individual setters for Comment model and refactor fields

--- src/main/java/ie/comment/Comment.java
+++ src/main/java/ie/comment/Comment.java
@@ -1,24 +1,48 @@
 package ie.comment;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import ie.film.Film;
+import ie.types.Constant;
+import ie.types.Email;
 import ie.user.User;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 public class Comment {
     private String id;
-    private Date createdDate;
-    private Film commentFilm;
-    private User commentOwner;
+    private LocalDate createdDate;
+    private String commentFilm;
+    private String commentOwner;
     private String text;
 
     @JsonCreator
-    public Comment(String id, Film commentFilm, User commentOwner, String text) {
+    private Comment() {}
+
+    @JsonProperty(value = Constant.Comment.ID, required = true)
+    private void setId(String id) {
         this.id = id;
-        this.createdDate = new Date();
-        this.commentOwner = commentOwner;
-        this.commentFilm = commentFilm;
+    }
+
+    @JsonProperty(value = Constant.Comment.M_ID, required = true)
+    private void setCommentFilm(String id) {
+        this.commentFilm = id;
+    }
+
+    @JsonProperty(value = Constant.Comment.U_ID, required = true)
+    private void setCommentOwner(String id) {
+        this.commentOwner = id;
+    }
+
+    @JsonProperty(value = Constant.Comment.CONTENT, required = true)
+    private void setText(String text) {
         this.text = text;
     }
+
+    @JsonProperty(value = Constant.Comment.C_DATE, required = true)
+    private void setCreatedDate(String createdDate){
+        this.createdDate = LocalDate.parse(createdDate);
+    }
+
 }
