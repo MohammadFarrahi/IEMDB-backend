16183711fada80e75fb319f0a30a0fa0a9770223
Mohammad Farrahi <farahim.1379@gmail.com>
2022-03-01 17:20:46 +0330

    Add generic ModelManager

---
+++ src/main/java/ie/generic/model/Manager.java
@@ -0,0 +1,34 @@
+package ie.generic.model;
+
+import ie.exception.CustomException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public abstract class Manager <T> {
+    private Map<String,T> objectMap;
+
+    public abstract String addElement(T object);
+    public abstract String updateElement(String id, T newObject);
+
+    public T getElementById(String id) throws CustomException {
+        if (!objectMap.containsKey(id)) {
+            throw new CustomException("ObjectNotFound");
+        }
+        return objectMap.get(id);
+    }
+    public List<T> getElementsById(List<String> ids) throws CustomException {
+        List<T> objects = new ArrayList<>();
+        for (String id : ids) {
+            objects.add(getElementById(id));
+        }
+        return objects;
+    }
+    public boolean isIdValid(String id) {
+        return objectMap.containsKey(id);
+    }
+    public boolean isIdListValid(List<String> ids) {
+        return objectMap.keySet().containsAll(ids);
+    }
+}
