c852f363af6886f95eb7e53ab79531a4849acb0f
Mohammad Farrahi <farahim.1379@gmail.com>
2022-03-04 01:13:49 +0330

    Add ActorRouter.java

---
+++ src/main/java/ie/app/actor/ActorRouter.java
@@ -0,0 +1,24 @@
+package ie.app.actor;
+
+import ie.generic.router.Router;
+import io.javalin.Javalin;
+import static io.javalin.apibuilder.ApiBuilder.get;
+import static io.javalin.apibuilder.ApiBuilder.path;
+
+public class ActorRouter extends Router {
+    private ActorController controller;
+
+    public ActorRouter() { controller = new ActorController(); }
+    @Override
+    public void addRoutes(Javalin javalin) {
+        javalin.routes(() -> {
+            path(UrlsPath.ACTORS + UrlsPath.A_ID, () -> {
+                get(controller::actorHandler);
+            });
+        });
+    }
+    public static class UrlsPath {
+        public static final String ACTORS = "/actors";
+        public static final String A_ID = "/{actor_id}";
+    }
+}
