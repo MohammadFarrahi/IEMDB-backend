8abd5b521ee414f17b1f4b9137f48df1280af2dd
Mohammad Farrahi <farahim.1379@gmail.com>
2022-03-04 06:20:18 +0330

    Add getWatchListResponse

--- src/main/java/ie/app/user/UserController.java
+++ src/main/java/ie/app/user/UserController.java
@@ -1,14 +1,17 @@
 package ie.app.user;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import ie.app.film.FilmManager;
 import ie.exception.CustomException;
 import ie.generic.controller.Controller;
 import io.javalin.http.Context;
 
+import java.io.IOException;
+
 public class UserController extends Controller {
     private UserView viewHandler;
     public UserController() { viewHandler = new UserView(); }
-    public void getWatchListHandler(Context ctx) throws CustomException {
+    public void getWatchListHandler(Context ctx) throws CustomException, IOException {
         // TODO : check userId with valid email format (?is it necessary?)
         var userId = ctx.pathParam(UserRouter.UrlsPath.U_ID);
         var user = UserManager.getInstance().getElementById(userId);
