057fb2fadff9265f538360c9e48658ccc3bc36aa
Mohammad Farrahi <farahim.1379@gmail.com>
2022-03-04 05:25:00 +0330

    Add UserRouter and watchList route

--- src/main/java/ie/app/user/UserController.java
+++ src/main/java/ie/app/user/UserController.java
@@ -1,6 +1,11 @@
 package ie.app.user;
 
 import ie.generic.controller.Controller;
+import io.javalin.http.Context;
 
 public class UserController extends Controller {
+    private UserView viewHandler;
+    public UserController() { viewHandler = new UserView(); }
+    public void getWatchListHandler(Context context) {
+    }
 }
---
+++ src/main/java/ie/app/user/UserRouter.java
@@ -0,0 +1,26 @@
+package ie.app.user;
+
+import ie.generic.router.Router;
+import io.javalin.Javalin;
+
+import static io.javalin.apibuilder.ApiBuilder.*;
+
+public class UserRouter extends Router {
+    private UserController controller;
+    public UserRouter() { this.controller = new UserController(); }
+    @Override
+    public void addRoutes(Javalin javalin) {
+        javalin.routes(() -> {
+            path(UrlsPath.W_LIST, () -> {
+                post(controller::postWatchListItemHandler);
+                path(UrlsPath.U_ID, () -> {
+                    get(controller::getWatchListHandler);
+                });
+            });
+        });
+    }
+    public static class UrlsPath {
+        public static final String W_LIST = "/watchList";
+        public static final String U_ID = "{user_id}";
+    }
+}
