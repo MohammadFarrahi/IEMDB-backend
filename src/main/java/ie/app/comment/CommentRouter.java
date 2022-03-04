b1b8e285dfa19daa33aa2d0e36c33413d4f18040
Mohammad Farrahi <farahim.1379@gmail.com>
2022-03-04 18:03:51 +0330

    Add CommentRouter and set /voteCommet route

---
+++ src/main/java/ie/app/comment/CommentRouter.java
@@ -0,0 +1,31 @@
+package ie.app.comment;
+
+import ie.generic.router.Router;
+import io.javalin.Javalin;
+
+import static io.javalin.apibuilder.ApiBuilder.*;
+
+public class CommentRouter extends Router {
+    private CommentController controller;
+
+    public CommentRouter() { controller = new CommentController(); }
+    @Override
+    public void addRoutes(Javalin javalin) {
+        javalin.routes(() -> {
+            path(UrlsPath.VCOMMENT, () -> {
+                path(UrlsPath.C_ID, () -> {
+//                    post(controller::voteCommentHandler);
+                });
+                path(UrlsPath.U_ID + UrlsPath.C_ID + UrlsPath.VOTE, () -> {
+                    get(controller::voteCommentHandler);
+                });
+            });
+        });
+    }
+    public static class UrlsPath {
+        public static final String VCOMMENT = "/voteComment";
+        public static final String U_ID = "/{actor_id}";
+        public static final String C_ID = "/{comment_id}";
+        public static final String VOTE = "/{vote}";
+    }
+}
