4ed1e39e5f224fbf4c883078cd9931f24b935bd4
Mohammad Farrahi <farahim.1379@gmail.com>
2022-03-05 15:59:19 +0330

    Add testWatchListSuccess

--- src/test/java/ie/IemdbTest.java
+++ src/test/java/ie/IemdbTest.java
@@ -1,10 +1,13 @@
 package ie;
 
+import ie.app.film.FilmManager;
+import ie.app.user.UserManager;
 import ie.exception.*;
+import ie.generic.model.JsonHandler;
 import ie.util.types.Constant;
-import org.jsoup.Connection;
 import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,7 +18,7 @@ import static org.junit.Assert.*;
 
 public class IemdbTest {
     Iemdb iemdb = null;
-    Connection.Response response = null;
+    Document responseBody = null;
 
     @Before
     public void setup() throws CustomException {
@@ -29,7 +32,7 @@ public class IemdbTest {
         iemdb.removeDatabase();
         iemdb.stopServer();
         iemdb = null;
-        response = null;
+        responseBody = null;
     }
 
     public void assert404Response(int code) {
@@ -39,7 +42,10 @@ public class IemdbTest {
         assertEquals(403, code);
     }
     public void assertHtmlValue(String htmlElementId, String expectedText) throws IOException {
-        assertEquals(expectedText, response.parse().getElementById(htmlElementId).text());
+        assertEquals(expectedText, responseBody.getElementById(htmlElementId).text());
+    }
+    public void assertHtmlValue(String htmlElementName, int elementIndex, String expectedText) throws IOException {
+        assertEquals(expectedText, responseBody.select(htmlElementName).get(elementIndex).text());
     }
 
     @Test
@@ -48,18 +54,18 @@ public class IemdbTest {
         var films = Iemdb.filmIds;
         Jsoup.connect(Constant.Server.BASE + "/rateMovie/" + users.get(0) + '/' + films.get(1) + "/8").execute();
         Jsoup.connect(Constant.Server.BASE + "/rateMovie/" + users.get(1) + '/' + films.get(1) + "/7").execute();
-        response = Jsoup.connect(Constant.Server.BASE + "/movies/" + films.get(1)).execute();
+        responseBody = Jsoup.connect(Constant.Server.BASE + "/movies/" + films.get(1)).execute().parse();
         assertHtmlValue("rating", "rating:7.5");
 
         // updating rate
         Jsoup.connect(Constant.Server.BASE + "/rateMovie/" + users.get(1) + '/' + films.get(1) + "/9").execute();
-        response = Jsoup.connect(Constant.Server.BASE + "/movies/" + films.get(1)).execute();
+        responseBody = Jsoup.connect(Constant.Server.BASE + "/movies/" + films.get(1)).execute().parse();
         assertHtmlValue("rating", "rating:8.5");
     }
     public void testRateMovieInvalidRate() throws IOException {
         var users = Iemdb.userIds;
         var films = Iemdb.filmIds;
-        response = Jsoup.connect(Constant.Server.BASE + "/rateMovie/" + users.get(0) + '/' + films.get(1) + "/12").execute();
+        responseBody = Jsoup.connect(Constant.Server.BASE + "/rateMovie/" + users.get(0) + '/' + films.get(1) + "/12").execute().parse();
 
     }
     public void testRateMovieInvalidId() throws IOException {
@@ -73,5 +79,20 @@ public class IemdbTest {
         assert403Response(e.getStatusCode());
         e = assertThrows(HttpStatusException.class, this::testRateMovieInvalidId);
         assert404Response(e.getStatusCode());
+    }
+    @Test
+    public void testWatchListSuccess() throws IOException, CustomException {
+        var userJson = JsonHandler.getNodeOfObject(UserManager.getInstance().getElementById(iemdb.userIds.get(0)));
+        var filmJson = JsonHandler.getNodeOfObject(FilmManager.getInstance().getElementById(iemdb.filmIds.get(0)));
+
+        var userId = userJson.get(Constant.User.E_ID).asText();
+        var userNickname = userJson.get(Constant.User.NICKNAME).asText();
+        var filmId = filmJson.get(Constant.Movie.ID_G).asText();
+        var filmName = filmJson.get(Constant.Movie.NAME).asText();
+
+        Jsoup.connect(Constant.Server.BASE + "/watchList/" + userId + '/' + filmId).execute();
+        responseBody = Jsoup.connect(Constant.Server.BASE + "/watchList/" + userId).execute().parse();
+        assertHtmlValue("nickname", "nickname: @" + userNickname);
+        assertHtmlValue("td", 0, filmName);
     }
 }
