eda4db7c95fcb21917b0f8a0b9e171a629631417
Mohammad Farrahi <farahim.1379@gmail.com>
2022-04-23 23:33:08 +0430

    Create spring project

---
+++ src/main/java/ie/iemdb/IemdbBackendApplication.java
@@ -0,0 +1,13 @@
+package ie.iemdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IemdbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IemdbBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/iemdb/IemdbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.iemdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IemdbBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
