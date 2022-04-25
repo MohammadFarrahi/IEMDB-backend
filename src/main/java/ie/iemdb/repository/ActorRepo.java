799fa76c3d2c9495e91e84dec963cd2ea4bc9e18
Soroosh Mirzasarvari <sarvisoroosh@gmail.com>
2022-04-25 10:52:49 +0430

    Refactor ActorRepo to work with entity rather than id

--- src/main/java/ie/iemdb/repository/ActorRepo.java
+++ src/main/java/ie/iemdb/repository/ActorRepo.java
@@ -1,13 +1,13 @@
 package ie.iemdb.repository;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+
+import ie.iemdb.exception.ActorAlreadyExistsException;
 import ie.iemdb.exception.ActorNotFoundException;
 import ie.iemdb.exception.CustomException;
 import ie.iemdb.model.Actor;
 import ie.iemdb.util.types.Constant;
 
-
-import java.util.ArrayList;
 
 public class ActorRepo extends Repo<Actor> {
     private static ActorRepo instance = null;
@@ -24,21 +24,19 @@ public class ActorRepo extends Repo<Actor> {
     }
 
     @Override
-    public String addElement(Actor newObject) throws CustomException {
+    public void addElement(Actor newObject) throws CustomException {
         var objectId = newObject.getId().toString();
         if (isIdValid(objectId)) {
-            throw new CustomException("ObjectAlreadyExists"); // TODO : customize message
+            throw new ActorAlreadyExistsException();
         }
         this.objectMap.put(objectId, newObject);
-        return objectId;
     }
+
     @Override
-    public String updateElement(Actor newObject) throws ActorNotFoundException {
+    public void updateElement(Actor newObject) throws ActorNotFoundException {
         var objectId = newObject.getId().toString();
         if (!isIdValid(objectId)) {
             throw new ActorNotFoundException();
         }
         objectMap.put(objectId, newObject);
-        return objectId;
-    }
-}
+    }}
