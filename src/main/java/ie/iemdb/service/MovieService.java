bccdbf4dbfb705a41bac07a7c8f47df544a400cc
Mohammad Farrahi <farahim.1379@gmail.com>
2022-04-25 18:51:53 +0430

    Rename MoviesService to MovieService

--- src/main/java/ie/iemdb/service/MovieService.java
+++ src/main/java/ie/iemdb/service/MovieService.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-public class MoviesService {
+public class MovieService {
     @RequestMapping(value = "/movies", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<MovieBriefDTO> getMoviesList() throws ObjectNotFoundException {
         // TODO : validation and exception handling
