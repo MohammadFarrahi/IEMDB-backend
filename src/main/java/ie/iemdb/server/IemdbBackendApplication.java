78f5627fdb8403c8ee058b4fb88d551c7caee5eb
Mohammad Farrahi <farahim.1379@gmail.com>
2022-04-25 18:31:03 +0430

    Add some server config

--- src/main/java/ie/iemdb/server/IemdbBackendApplication.java
+++ src/main/java/ie/iemdb/server/IemdbBackendApplication.java
@@ -1,12 +1,17 @@
 package ie.iemdb.server;
 
+import ie.iemdb.exception.CustomException;
+import ie.iemdb.model.Actor;
+import ie.iemdb.repository.ActorRepo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "ie.iemdb.service")
 public class IemdbBackendApplication {
-
-	public static void main(String[] args) {
+	public static void main(String[] args) throws CustomException {
+		ActorRepo.getInstance().addElement(new Actor("1", "a", "1999-09-09", "hello", "sldkjf"));
 		SpringApplication.run(IemdbBackendApplication.class, args);
 	}
 
