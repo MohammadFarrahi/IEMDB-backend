8f40b79fc7fbaeb8f1d4df2d09f03add605fc204
Mohammad Farrahi <farahim.1379@gmail.com>
2022-04-27 15:59:05 +0430

    Handle ActorService exceptions

--- src/main/java/ie/iemdb/service/ActorService.java
+++ src/main/java/ie/iemdb/service/ActorService.java
@@ -3,16 +3,22 @@ package ie.iemdb.service;
 import ie.iemdb.domain.ActorDomainManager;
 import ie.iemdb.exception.ObjectNotFoundException;
 import ie.iemdb.model.DTO.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ActorService {
     // TODO : validation and exception handling
     @RequestMapping(value = "/actors/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Response getActorInfo(@PathVariable(value = "id") String actorId) throws ObjectNotFoundException {
-        return new Response(true, "okeb", ActorDomainManager.getInstance().getActorDTO(actorId));
+    public Response getActorInfo(@PathVariable(value = "id") String actorId) {
+        try {
+            return new Response(true, "okeb", ActorDomainManager.getInstance().getActorDTO(actorId));
+        } catch (ObjectNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Actor Not Found", e);
+        }
     }
 }
 
