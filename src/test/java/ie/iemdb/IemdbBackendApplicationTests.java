4f4a5900546c088995c38b37e9b9db6bb11eb409
Soroosh Mirzasarvari <sarvisoroosh@gmail.com>
2022-05-30 02:42:44 +0430

    Add Dockerfile

--- src/test/java/ie/iemdb/IemdbBackendApplicationTests.java
+++ src/test/java/ie/iemdb/IemdbBackendApplicationTests.java
@@ -1,9 +1,10 @@
 package ie.iemdb;
 
+import ie.iemdb.server.IemdbBackendApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = IemdbBackendApplication.class)
 class IemdbBackendApplicationTests {
 
 	@Test
